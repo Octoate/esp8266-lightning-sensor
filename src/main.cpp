7cb212be390d7ff50109f288a7572d58749ec4f8
Tim Riemann <triemann@octoate.de>
2020-06-16 21:17:44 +0200

    add WiFiManager and ArduinoJson library

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include <Arduino.h>
+
+// file system access for configuration storage
+#include <FS.h>
+
+#include <ESP8266WiFi.h>
 #include <Wire.h>
+
+//needed for the WiFiManager library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
+// JSON support
+#include <ArduinoJson.h> 
 
 void setup() {
   // put your setup code here, to run once:
